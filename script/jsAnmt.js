83d84c58d70ac6c90ce7953f1a37d87a3a818677
Pontim56 <leonardopontimp@hotmail.com>
2023-11-16 22:12:31 -0300

    tse

--- script/jsAnmt.js
+++ script/jsAnmt.js
@@ -34,13 +34,20 @@ function sobreF(){
 
 function checkBoxChange() {
     var checkBox = document.getElementById("meuCheckBox");
+    var continer = document.querySelector('main');
     
     if (checkBox.checked) {
         document.documentElement.style.setProperty('--fundo','rgb(255, 214, 157)');
         document.documentElement.style.setProperty('--fundo2','rgb(255, 193, 108)');
         document.documentElement.style.setProperty('--fundo3','rgb(255, 148, 48)');
         document.documentElement.style.setProperty('--destaq','rgb(248, 56, 57)');
+        // continer.classList.add('animate__animated', 'animate__lightSpeedInLeft');
+        continer.classList.add('animate__animated', 'animate__shakeX','animate__faster','animate__infinite');
+        // continer.classList.add('animate__animated', 'animate__tada');
     } else {
+        // continer.classList.remove('animate__animated', 'animate__lightSpeedInLeft');
+        continer.classList.remove('animate__animated', 'animate__shakeX','animate__faster', 'animate__infinite');
+        // continer.classList.remove('animate__animated', 'animate__tada');
         document.documentElement.style.setProperty('--fundo','rgb(207, 229, 236)');
         document.documentElement.style.setProperty('--fundo2','rgb(173,216,230)');
         document.documentElement.style.setProperty('--fundo3','rgb(134, 197, 218)');
